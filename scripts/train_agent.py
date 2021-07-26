abb29a52bed5620969f7eeb919f92d7ea010f2f9
WanyongFeng <46601448+WanyongFeng@users.noreply.github.com>
2021-07-26 12:21:58 +0800

    Update train_agent.py

--- scripts/train_agent.py
+++ scripts/train_agent.py
@@ -61,7 +61,7 @@ if args.mode == 'resume':
     agent.train()
 if args.mode == 'train':
     agent.train()
-train_dict = agent.evaluate(hard=args.hard, max_batches=45)
+train_dict = agent.evaluate(hard=args.hard, max_batches=30)
 
 # test
 with tf.device(f"/gpu:{args.env_device}"):
@@ -70,7 +70,7 @@ with tf.device(f"/gpu:{args.env_device}"):
 with tf.device(f"/gpu:{args.agent_device}"):
     agent = PPOPolicy(params, env, 'test')
 
-test_dict = agent.evaluate(hard=args.hard, max_batches=11)
+test_dict = agent.evaluate(hard=args.hard, max_batches=10)
 
 # save
 os.makedirs(f'{params.exp_dir}/evaluate', exist_ok=True)
